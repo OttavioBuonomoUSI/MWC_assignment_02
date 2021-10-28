5c0a0824471b47bc466f514838e612cbbfc6b53d
OttavioBuonomoUSI <91184854+OttavioBuonomoUSI@users.noreply.github.com>
2021-10-28 16:05:49 +0200

    Update comments

--- StepApp/app/src/main/java/com/example/stepapp/ui/report/DayFragment.java
+++ StepApp/app/src/main/java/com/example/stepapp/ui/report/DayFragment.java
@@ -47,10 +47,8 @@ public class DayFragment extends Fragment {
             container.removeAllViews();
         }
 
-        // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_day, container, false);
 
-        // Create column chart
         anyChartView = root.findViewById(R.id.dayBarChart);
         anyChartView.setProgressBar(root.findViewById(R.id.loadingBarDay));
 
@@ -65,48 +63,33 @@ public class DayFragment extends Fragment {
         //***** Read data from SQLiteDatabase *********/
         stepsByDay = StepAppOpenHelper.loadStepsByDay(getContext());
 
-
         //***** Create column chart using AnyChart library *********/
-        // 1. Create and get the cartesian coordinate system for column chart
         Cartesian cartesian = AnyChart.column();
-
-        // 2. Create data entries for x and y axis of the graph
         List<DataEntry> data = new ArrayList<>();
 
         for (Map.Entry<String,Integer> entry : stepsByDay.entrySet())
             data.add(new ValueDataEntry(entry.getKey(), entry.getValue()));
-
-        // 3. Add the data to column chart and get the columns
         Column column = cartesian.column(data);
 
         //***** Modify the UI of the chart *********/
-        // TODO 4. Change the color of column chart and its border
         column.fill("#1EB980");
         column.stroke("#1EB980");
 
-
-        //Modifying properties of tooltip
         column.tooltip()
                 .titleFormat("At hour: {%X}")
                 .format("{%Value}{groupsSeparator: } Steps")
                 .anchor(Anchor.RIGHT_TOP);
 
-        // TODO 5: Modify column chart tooltip properties
+
         column.tooltip().position(Position.RIGHT_TOP).offsetX(0d).offsetY(5);
 
-
-        // Modifying properties of cartesian
         cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
         cartesian.interactivity().hoverMode(HoverMode.BY_X);
         cartesian.yScale().minimum(0);
-
-
-        // TODO 6: Modify the UI of the cartesian
         cartesian.background().fill("#00000000");
         cartesian.xAxis(0).title("Number of steps");
         cartesian.yAxis(0).title("Hours");
         cartesian.animation(true);
-
 
         return cartesian;
     }
